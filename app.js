1d4deb7714196df0ab8adbdccf2e6acbf926551e
Academy Student <academy.student@lafosse.com>
2026-03-10 14:52:31 +0000

    Added cors to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const fruitsApp = express()
 const fruitRoutes = require('./routes/fruitRoutes')
 
+fruitsApp.use(cors())
 fruitsApp.use(express.json)
 fruitsApp.use('/fruits', fruitRoutes)
 
